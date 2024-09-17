3f766b8e259ea7089b90177c73507dc0c83b8885
jeevithabhas <jeevithabhas20@gmail.com>
2024-09-17 15:55:17 +0530

    changes commited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,12 @@ const Navbar = ({ cartCount, openCart }) => {
     return (
         <nav className=" flex justify-between items-center p-4 bg-gradient-to-r from-black to-gray-300 text-sky-200">
             <h1 className="flex  text-5xl hover:text-neutral-400 font-bold italic hover:line-through capitalize hover:uppercase bg-gradient-to-r from-indigo-400 to-gray-950 text-pretty decoration-slate-50 decoration-4 tracking-tighter hover:tracking-wide font-mono">VogueStyle...
-            
             </h1>
              <button onClick={openCart} className="bg-gray-300 shadow-lg text-black fon text-xl px-4 py-4 rounded hover:bg-zinc-500 hover:pointer">Cart:🛒({cartCount})
-                
-            </button>
-            
-        </nav>
-        
-    );
-};
+                </button>
+            </nav>
+           );
+           };
 
 export default Navbar;
 
